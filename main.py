16a21b61cbe4f668081cb28dfbef4b00bd399a9b
Marrek Nožka <nozka@spseol.cz>
2021-12-18 18:38:41 +0100

    řešení s tkinterovkými proměnnými

--- main.py
+++ main.py
@@ -2,7 +2,19 @@
 
 from os.path import basename, splitext
 import tkinter as tk
-from tkinter import Label, Button, Scale, HORIZONTAL, Canvas
+from tkinter import (
+    Label,
+    Button,
+    Scale,
+    HORIZONTAL,
+    Canvas,
+    Frame,
+    Entry,
+    LEFT,
+    S,
+    StringVar,
+    IntVar,
+)
 
 # from tkinter import ttk
 
@@ -17,46 +29,101 @@ class Application(tk.Tk):
 
         self.bind("<Escape>", self.quit)
 
-        self.lblR = Label(self, text="R")
-        self.lblR.pack()
+        # R
+        self.varR = IntVar()
+        self.frameR = Frame(self)
+        self.frameR.pack()
+        self.lblR = Label(self.frameR, text="R")
+        self.lblR.pack(side=LEFT, anchor=S)
         self.scaleR = Scale(
-            from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change
+            self.frameR,
+            from_=0,
+            to=255,
+            orient=HORIZONTAL,
+            length=256,
+            variable=self.varR,
         )
-        self.scaleR.pack()
+        self.scaleR.pack(side=LEFT, anchor=S)
+        self.entryR = Entry(self.frameR, width=5, textvariable=self.varR)
+        self.entryR.pack(side=LEFT, anchor=S)
 
-        self.lblG = Label(self, text="G")
-        self.lblG.pack()
+        # G
+        self.varG = IntVar()
+        self.frameG = Frame(self)
+        self.frameG.pack()
+        self.lblG = Label(self.frameG, text="G")
+        self.lblG.pack(side=LEFT, anchor=S)
         self.scaleG = Scale(
-            from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change
+            self.frameG,
+            from_=0,
+            to=255,
+            orient=HORIZONTAL,
+            length=256,
+            variable=self.varG,
         )
-        self.scaleG.pack()
+        self.scaleG.pack(side=LEFT, anchor=S)
+        self.entryG = Entry(self.frameG, width=5, textvariable=self.varG)
+        self.entryG.pack(side=LEFT, anchor=S)
 
-        self.lblB = Label(self, text="B")
-        self.lblB.pack()
+        # B
+        self.varB = IntVar()
+        self.frameB = Frame(self)
+        self.frameB.pack()
+        self.lblB = Label(self.frameB, text="B")
+        self.lblB.pack(side=LEFT, anchor=S)
         self.scaleB = Scale(
-            from_=0, to=255, orient=HORIZONTAL, length=256, command=self.change
+            self.frameB,
+            from_=0,
+            to=255,
+            orient=HORIZONTAL,
+            length=256,
+            variable=self.varB,
         )
-        self.scaleB.pack()
+        self.scaleB.pack(side=LEFT, anchor=S)
+        self.entryB = Entry(self.frameB, width=5, textvariable=self.varB)
+        self.entryB.pack(side=LEFT, anchor=S)
 
-        self.canvasMain = Canvas(width=256, height=100, background="#000000")
+        self.canvasMain = Canvas(self, width=277, height=200, background="#000000")
         self.canvasMain.pack()
 
-        self.btn = Button(self, text="Quit", command=self.quit)
+        self.varMain = StringVar()
+        self.entryMain = Entry(
+            self,
+            textvariable=self.varMain,
+            state="readonly",
+            readonlybackground="#ffffff",
+        )
+        self.entryMain.pack()
 
-        # self.btn.pack()
+        self.btnQ = Button(self, text="Quit", command=self.quit)
+        # self.btnQ.pack()
 
-        self.btn2 = Button(self, text="About", command=self.quit)
-        # self.btn2.pack()
+        """Proměnné varRGB jsou spojené jak s Scale tak s Entry, takže stačí sledovat
+        změnu v těchto proměných"""
+        self.varR.trace("w", self.change)
+        self.varG.trace("w", self.change)
+        self.varB.trace("w", self.change)
 
-    def change(self, event):
-        self.lblG.config(text="ahoj")
+    def change(self, var, index, mode):
+        # print(type(var))
+        # print(var)
+        # print(dir(var))
+        # print(index)
+        # print(dir(index))
+        # print(mode)
+        # print(dir(mode))
 
-        r = self.scaleR.get()
-        g = self.scaleG.get()
-        b = self.scaleB.get()
-        self.canvasMain.config(background=f"#{r:02x}{g:02x}{b:02x}")
-        #print(f"#{r:2x}{g:2x}{b:2x}")
-        #print(f"#{r:02x}{g:02x}{b:02x}")
+        r = int(self.varR.get())
+        g = int(self.varG.get())
+        b = int(self.varB.get())
+        colorstring = f"#{r:02X}{g:02X}{b:02X}"
+        self.canvasMain.config(background=colorstring)
+        self.varMain.set(colorstring)
+        # print(colorstring)
+
+        # self.varR.set(r)
+        # self.varG.set(g)
+        # self.varB.set(b)
 
     def quit(self, event=None):
         super().quit()
